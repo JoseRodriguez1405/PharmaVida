7498f966b78f711befd8d547d1f4ae5e6c426302
JoseRodriguez1405 <timoteo1405@outlook.com>
2024-05-28 00:56:03 -0500

    28-05-2024

---
+++ js/filtro.js
@@ -0,0 +1,564 @@
+let Produc = [
+    {
+        NombreProduc: "Acetaminofen",
+        Categoria: "Medicamentos",
+        Codigo: "Acc00001",
+        Precio: 1500,
+        Laboratorio: "MK",
+        Cantidad: "8 tabletas",
+        Concentrado: "352 mg",
+        Imagen: "../img/acetaminofen.webp",
+},
+{
+    NombreProduc: "Dolex niños",
+        Categoria: "Medicamento",
+        Codigo: "Acc00002",
+        Precio: 14500,
+        Laboratorio: "Glaxos",
+        Cantidad: "1 frasco",
+        Concentrado: "90 ml",
+        Imagen: "../img/dolex_niños.jpeg",
+},
+{
+    NombreProduc: "Sevedol Extra Fuerte",
+        Categoria: "Medicamentos",
+        Codigo: "Acc00003",
+        Precio: 11400,
+        Laboratorio: "LaFrancol",
+        Cantidad: "8 tabletas",
+        Concentrado: "400 mg",
+        Imagen: "../img/sevedol.webp",
+},
+{
+    NombreProduc: "Vitamina C",
+        Categoria: "Medicamento",
+        Codigo: "Acc00004",
+        Precio: 5300,
+        Laboratorio: "MK",
+        Cantidad: "20 tabletas",
+        Concentrado: "500 mg",
+        Imagen: "../img/vitamina_c.jpeg",
+},
+{
+    NombreProduc: "Dolex Rapida Accion",
+        Categoria: "Medicamento",
+        Codigo: "Acc00005",
+        Precio: 21000,
+        Laboratorio: "Glaxos",
+        Cantidad: "24 tabletas",
+        Concentrado: "500 mg",
+        Imagen: "../img/dolex_rapida_accion.jpeg",
+},
+    {
+    NombreProduc: "Dolex Forte",
+        Categoria: "Medicamento",
+        Codigo: "Acc00006",
+        Precio: 20000,
+        Laboratorio: "Glaxos",
+        Cantidad: "14 tabletas",
+        Concentrado: "500 mg",
+        Imagen: "../img/dolex_forte.jpg",
+},
+{
+    NombreProduc: "Dolex Gripa",
+        Categoria: "Medicamento",
+        Codigo: "Acc00007",
+        Precio: 15000,
+        Laboratorio: "Glaxos",
+        Cantidad: "12 tabletas",
+        Concentrado: "400 mg",
+        Imagen: "../img/dolex_gripa.jpeg",
+},
+{
+    NombreProduc: "Pax Noche",
+        Categoria: "Medicamento",
+        Codigo: "Acc00008",
+        Precio: 11000,
+        Laboratorio: "Sanofi Aventis",
+        Cantidad: "6 sobres",
+        Concentrado: "500 mg",
+        Imagen: "../img/pax_noche.jpeg",
+},
+{
+    NombreProduc: "Pax Dia",
+        Categoria: "Medicamento",
+        Codigo: "Acc00009",
+        Precio: 11000,
+        Laboratorio: "Glaxos",
+        Cantidad: "6 tabletas",
+        Concentrado: "500 mg",
+        Imagen: "../img/pax_dia.jpg",
+},
+{
+    NombreProduc: "Gelicart Advance",
+        Categoria: "Medicamento",
+        Codigo: "Acc00010",
+        Precio: 113000,
+        Laboratorio: "Glaxos",
+        Cantidad: "30 sobres",
+        Concentrado: "600 Gr",
+        Imagen: "../img/gelicart_advance.jpeg",
+},
+{
+    NombreProduc: "Ensure Advance Vainilla",
+        Categoria: "Nutricion",
+        Codigo: "Acc00011",
+        Precio: 70000,
+        Laboratorio: "Ensure",
+        Cantidad: "1 lata",
+        Concentrado: "400 gr",
+        Imagen: "../img/ensure_vainilla.jpeg",
+},
+{
+    NombreProduc: "Pediasure Chocolate",
+        Categoria: "Nutricion",
+        Codigo: "Acc00012",
+        Precio: 10000,
+        Laboratorio: "Pediasure",
+        Cantidad: "1 tarro",
+        Concentrado: "220 ml",
+        Imagen: "../img/pediasure_chocolate.webp",
+},
+{
+    NombreProduc: "Prowhey Proteina Plus",
+        Categoria: "Nutricion",
+        Codigo: "Acc00013",
+        Precio: 115000,
+        Laboratorio: "Prowhey",
+        Cantidad: "1 lata",
+        Concentrado: "320 gr",
+        Imagen: "../img/prowhey_plus.jpeg",
+},
+{
+    NombreProduc: "Prowhey Proteina",
+        Categoria: "Medicamento",
+        Codigo: "Acc00014",
+        Precio: 11000,
+        Laboratorio: "Prowhey",
+        Cantidad: "1 lata",
+        Concentrado: "320 gr",
+        Imagen: "../img/prowhey_proteina.jpeg",
+},
+{
+    NombreProduc: "Nutren Senior",
+        Categoria: "Nutricion",
+        Codigo: "Acc00015",
+        Precio: 108000,
+        Laboratorio: "Nutren",
+        Cantidad: "1 lata",
+        Concentrado: "740 gr",
+        Imagen: "../img/nutren_senior.jpeg",
+},
+{
+    NombreProduc: "Ensure Advance Chocolate",
+        Categoria: "Nutricion",
+        Codigo: "Acc00016",
+        Precio: 9000,
+        Laboratorio: "Ensure",
+        Cantidad: "1 tarro",
+        Concentrado: "220 ml",
+        Imagen: "../img/ensure_chocolate.jpeg",
+},
+{
+    NombreProduc: "Ensure Advance Fresa",
+        Categoria: "Nutricion",
+        Codigo: "Acc00017",
+        Precio: 9000,
+        Laboratorio: "Ensure",
+        Cantidad: "1 tarro",
+        Concentrado: "220 ml",
+        Imagen: "../img/ensure_fresa.jpeg",
+},
+{
+    NombreProduc: "Hydro Fluid Eucerin",
+        Categoria: "Protector Solar",
+        Codigo: "Acc00018",
+        Precio: 120000,
+        Laboratorio: "Eucerin",
+        Cantidad: "1 frasco",
+        Concentrado: "50 ml",
+        Imagen: "../img/hydro_fluid.jpeg",
+},
+{
+    NombreProduc: "Fluido SPF",
+        Categoria: "Protector Solar",
+        Codigo: "Acc00019",
+        Precio: 81000,
+        Laboratorio: "Avene",
+        Cantidad: "1 tubo",
+        Concentrado: "50 ml",
+        Imagen: "../img/fluido_spf.jpeg",
+},
+{
+    NombreProduc: "Nude Touch Photoderm",
+        Categoria: "Protector Solar",
+        Codigo: "Acc00020",
+        Precio: 81000,
+        Laboratorio: "Bioderma",
+        Cantidad: "1 frasco",
+        Concentrado: "40 ml",
+        Imagen: "../img/photoderm.jpeg",
+},
+{
+    NombreProduc: "Intense Protect",
+        Categoria: "Protector Solar",
+        Codigo: "Acc00021",
+        Precio: 123000,
+        Laboratorio: "Avene",
+        Cantidad: "1 tubo",
+        Concentrado: "50 ml",
+        Imagen: "../img/intense_protect.jpeg",
+},
+{
+    NombreProduc: "Mineral 89",
+        Categoria: "Cuidado Facial",
+        Codigo: "Acc00022",
+        Precio: 192000,
+        Laboratorio: "Vichy",
+        Cantidad: "1 frasco",
+        Concentrado: "50 ml",
+        Imagen: "../img/mineral_89.jpeg",
+},
+{
+    NombreProduc: "Cetaphil-AC",
+        Categoria: "Cuidado Facial",
+        Codigo: "Acc00023",
+        Precio: 132000,
+        Laboratorio: "Cetaphil",
+        Cantidad: "1 frasco",
+        Concentrado: "118 ml",
+        Imagen: "../img/cetaphil_AC.jpeg",
+},
+{
+    NombreProduc: "Hydrance Optimale Serum",
+        Categoria: "Cuidado Facial",
+        Codigo: "Acc00024",
+        Precio: 137000,
+        Laboratorio: "Avene",
+        Cantidad: "1 frasco",
+        Concentrado: "30 ml",
+        Imagen: "../img/hydrance.jpeg",
+},
+{
+    NombreProduc: "Agua Termal",
+        Categoria: "Cuidado Facial",
+        Codigo: "Acc00025",
+        Precio: 73000,
+        Laboratorio: "Avene",
+        Cantidad: "1 frasco",
+        Concentrado: "300 ml",
+        Imagen: "../img/agua_termal.jpeg",
+},
+{
+    NombreProduc: "Shampoo Intensivo Anticaspa Kerium",
+        Categoria: "Cabello",
+        Codigo: "Acc00026",
+        Precio: 131000,
+        Laboratorio: "La Roche Posay",
+        Cantidad: "1 tarro",
+        Concentrado: "125 ml",
+        Imagen: "../img/shampoo_kerium.jpeg",
+},
+{
+    NombreProduc: "Shampoo Anti Caspa",
+        Categoria: "Cabello",
+        Codigo: "Acc00027",
+        Precio: 97000,
+        Laboratorio: "Dercos",
+        Cantidad: "1 tarro",
+        Concentrado: "200 ml",
+        Imagen: "../img/shampoo_dercos.jpeg",
+},
+{
+    NombreProduc: "Shampoo Estimulante Dercos",
+        Categoria: "Cabello",
+        Codigo: "Acc00028",
+        Precio: 97000,
+        Laboratorio: "Vichy",
+        Cantidad: "1 tarro",
+        Concentrado: "200 ml",
+        Imagen: "../img/shampoo_estimulante.jpeg",
+},
+{
+    NombreProduc: "Condones Today",
+        Categoria: "Salud Sexual",
+        Codigo: "Acc00029",
+        Precio: 17000,
+        Laboratorio: "Pfizer",
+        Cantidad: "3 unidades",
+        Concentrado: " ",
+        Imagen: "../img/today.jpeg",
+},
+{
+    NombreProduc: "Condones Duo Lubricado",
+        Categoria: "Salud Sexual",
+        Codigo: "Acc00030",
+        Precio: 16500,
+        Laboratorio: "Duo",
+        Cantidad: "6 unidades",
+        Concentrado: " ",
+        Imagen: "../img/duo.jpeg",
+},
+{
+    NombreProduc: "Gel Lubricante intimo K-Y",
+        Categoria: "Salud Sexual",
+        Codigo: "Acc00031",
+        Precio: 40928,
+        Laboratorio: "RB Health",
+        Cantidad: "1 tubo",
+        Concentrado: "100 gr",
+        Imagen: "../img/gel_lubricante.jpeg",
+},
+{
+    NombreProduc: "Pañitos Humedos Pequeñin",
+        Categoria: "Maternidad",
+        Codigo: "Acc00032",
+        Precio: 12000,
+        Laboratorio: "Pequeñin",
+        Cantidad: "100 unidades",
+        Concentrado: " ",
+        Imagen: "../img/pañitos.jpeg",
+},
+{
+    NombreProduc: "Copitos de Algodon",
+        Categoria: "Maternidad",
+        Codigo: "Acc00033",
+        Precio: 9800,
+        Laboratorio: "Suavex",
+        Cantidad: "150 unidades",
+        Concentrado: " ",
+        Imagen: "../img/copitos.jpeg",
+},
+{
+    NombreProduc: "Pañal Ultratrim etapa 4",
+        Categoria: "Maternidad",
+        Codigo: "Acc00034",
+        Precio: 62300,
+        Laboratorio: "Winny",
+        Cantidad: "50 unidades",
+        Concentrado: " ",
+        Imagen: "../img/pañal_winny.jpeg",
+},
+{
+    NombreProduc: "Pañal Huggies Xtra-Care",
+        Categoria: "Maternidad",
+        Codigo: "Acc00035",
+        Precio: 84000,
+        Laboratorio: "Huggies",
+        Cantidad: "50 unidades",
+        Concentrado: " ",
+        Imagen: "../img/pañal_huggies.jpeg",
+},
+{
+    NombreProduc: "Apronax Liquid Gels",
+        Categoria: "Medicamentos",
+        Codigo: "Acc00036",
+        Precio: 16800,
+        Laboratorio: "Bayer",
+        Cantidad: "8 capsulas",
+        Concentrado: "275 mg",
+        Imagen: "../img/apronax.jpeg",
+},
+{
+    NombreProduc: "Cafiaspirina Forte",
+        Categoria: "Medicamentos",
+        Codigo: "Acc00037",
+        Precio: 19000,
+        Laboratorio: "Cafiaspirina",
+        Cantidad: "12 tabletas",
+        Concentrado: "650 mg",
+        Imagen: "../img/cafiaspirina.jpeg",
+},
+{
+    NombreProduc: "Acetaminofen Forte",
+        Categoria: "Medicamentos",
+        Codigo: "Acc00038",
+        Precio: 50800,
+        Laboratorio: "MK",
+        Cantidad: "48 tabletas",
+        Concentrado: "565 mg",
+        Imagen: "../img/acetaminofen_forte.png",
+},
+{
+    NombreProduc: "Loratadina Jarabe",
+        Categoria: "Medicamentos",
+        Codigo: "Acc00039",
+        Precio: 13000,
+        Laboratorio: "Genfar",
+        Cantidad: "1 frasco",
+        Concentrado: "100 ml",
+        Imagen: "../img/loratadina.jpeg",
+},
+{
+    NombreProduc: "Betametasona MK",
+        Categoria: "Medicamentos",
+        Codigo: "Acc00040",
+        Precio: 21500,
+        Laboratorio: "Tecnoquimicas SA",
+        Cantidad: "1 ampolla",
+        Concentrado: "7 mg",
+        Imagen: "../img/betametasona.jpeg",
+},
+{
+    NombreProduc: "Prednisolona MK",
+        Categoria: "Medicamentos",
+        Codigo: "Acc00041",
+        Precio: 67450,
+        Laboratorio: "Tecnoquimicas SA",
+        Cantidad: "1 frasco",
+        Concentrado: "120 ml",
+        Imagen: "../img/prednisolona.jpg",
+},
+{
+    NombreProduc: "Gasa Esteril Nexcare",
+        Categoria: "Medicamentos",
+        Codigo: "Acc00042",
+        Precio: 14500,
+        Laboratorio: "3M",
+        Cantidad: "12 sobres",
+        Concentrado: " ",
+        Imagen: "../img/gasa.jpeg",
+},
+{
+    NombreProduc: "Repelente Stay Off",
+        Categoria: "Medicamentos",
+        Codigo: "Acc00043",
+        Precio: 14400,
+        Laboratorio: "Stay Off",
+        Cantidad: "1 frasco",
+        Concentrado: "40 gr",
+        Imagen: "../img/repelente.jpg",
+},
+{
+    NombreProduc: "Noxpirin Plus Adultos",
+        Categoria: "Medicamentos",
+        Codigo: "Acc00044",
+        Precio: 6500,
+        Laboratorio: "Siegfried",
+        Cantidad: "6 capsulas",
+        Concentrado: "500 mg",
+        Imagen: "../img/noxpirin.jpeg",
+},
+{
+    NombreProduc: "Vick VapoRub",
+        Categoria: "Medicamentos",
+        Codigo: "Acc00045",
+        Precio: 28000,
+        Laboratorio: "Vick",
+        Cantidad: "1 pote",
+        Concentrado: "100 gr",
+        Imagen: "../img/vick.jpeg",
+},
+{
+    NombreProduc: "Bellaface Suave",
+        Categoria: "Medicamentos",
+        Codigo: "Acc00046",
+        Precio: 13275,
+        Laboratorio: "Bellaface",
+        Cantidad: "28 tabletas",
+        Concentrado: "2 mg",
+        Imagen: "../img/bellaface.webp",
+},
+{
+    NombreProduc: "Suero Electrolif Sabor Uva",
+        Categoria: "Medicamentos",
+        Codigo: "Acc00047",
+        Precio: 8400,
+        Laboratorio: "Pisa Farmaceutica",
+        Cantidad: "1 frasco",
+        Concentrado: "625 ml",
+        Imagen: "../img/suero_uva.jpg",
+},
+{
+    NombreProduc: "Suero Electrolif Sabor Fresa",
+        Categoria: "Medicamentos",
+        Codigo: "Acc00048",
+        Precio: 8400,
+        Laboratorio: "Pisa Farmaceutica",
+        Cantidad: "1 frasco",
+        Concentrado: "625 ml",
+        Imagen: "../img/suero_fresa.jpeg",
+},
+{
+    NombreProduc: "Vaselina Pura",
+        Categoria: "Medicamentos",
+        Codigo: "Acc00049",
+        Precio: 21000,
+        Laboratorio: "Johnson & johnson",
+        Cantidad: "1 pote",
+        Concentrado: "100 gr",
+        Imagen: "../img/vaselina.jpeg",
+},
+{
+    NombreProduc: "Complejo B",
+        Categoria: "Medicamentos",
+        Codigo: "Acc00050",
+        Precio: 19980,
+        Laboratorio: "Pharmaris",
+        Cantidad: "30 capsulas",
+        Concentrado: " ",
+        Imagen: "../img/complejo_b.jpg",
+}
+]
+
+function CargarTabla(Produc){
+    const tablaBody = document.getElementById("produc_tabla").getElementsByTagName("tbody")[0];
+    tablaBody.innerHTML = '';
+
+    Produc.forEach(product => {
+        const fila = document.createElement("tr");
+
+        const nombreCelda = document.createElement("td");
+        nombreCelda.textContent = product.NombreProduc;
+        
+        const categoriaCelda = document.createElement("td");
+        categoriaCelda.textContent = product.Categoria;
+
+        const CodigoCelda = document.createElement("td");
+        CodigoCelda.textContent = product.Codigo;
+
+        const precioCelda = document.createElement("td");
+        precioCelda.textContent = product.Precio;
+        
+        const laboratorioCelda = document.createElement("td");
+        laboratorioCelda.textContent = product.Laboratorio;
+
+        const cantidadCelda = document.createElement("td");
+        cantidadCelda.textContent = product.Cantidad;
+
+        const concentradoCelda = document.createElement("td");
+        concentradoCelda.textContent = product.Concentrado;
+
+        const imagenCelda = document.createElement("td");
+        const Imagen = document.createElement("img");
+        Imagen.src = product.Imagen;
+        imagenCelda.appendChild(Imagen);
+        
+        fila.appendChild(nombreCelda);
+        fila.appendChild(categoriaCelda);
+        fila.appendChild(CodigoCelda);
+        fila.appendChild(precioCelda);
+        fila.appendChild(laboratorioCelda);
+        fila.appendChild(cantidadCelda);
+        fila.appendChild(concentradoCelda);
+        fila.appendChild(imagenCelda);
+        tablaBody.appendChild(fila);
+    });
+}
+
+function FiltrarTabla(){
+
+    const input = document.getElementById("Buscar").ariaValueMax.toLowerCase();
+    const filtrarProduc = Produc.filter(product => 
+        product.NombreProduc.toLowerCase().includes(input) ||
+        product.Categoria.toLowerCase().includes(input) ||
+        product.Codigo.toLowerCase().includes(input) ||
+        product.Precio.toLowerCase().includes(input) ||
+        product.Laboratorio.toLowerCase().includes(input) ||
+        product.Cantidad.toLowerCase().includes(input) ||
+        product.Concentrado.toLowerCase().includes(input)
+    );
+    CargarTabla(filtrarProduc);
+}
+
+CargarTabla(Produc);
